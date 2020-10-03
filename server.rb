d4c559104426fee1df9dcbe4e2c521bc3b8ae2a0
Egor Lenivin <egorlenivin@Egors-MacBook-Pro.local>
2020-10-03 15:18:51 +0400

    files, errors

--- server.rb
+++ server.rb
@@ -12,11 +12,41 @@ def handle_request(request_text, client)
   request  = Request.new(request_text)
   puts "#{client.peeraddr[3]} #{request.path}"
 
-  response = Response.new(code: 200, data: "Hello, world!")
+  response = route_request(request)
 
   response.send(client)
 
   client.shutdown
+end
+
+def route_request(request)
+  comand, query_params = request.path.split('?')
+  puts query_params.inspect, comand.inspect
+  path = query_params.split('=')[1]
+  data = case comand 
+  when '/ls'
+    dir_response(path)
+  when '/cat'
+    file_response(path)
+  end
+end
+
+def file_response(file_path)
+  if File.exist?(file_path)
+    data = File.read(file_path)
+    Response.new(code: 200, data: data)
+  else 
+    Response.new(code: 404, data: "File not found")
+  end
+end
+
+def dir_response(dir_path)
+  if File.exist?(dir_path)
+    data = `ls -la #{dir_path}`
+    Response.new(code: 200, data: data)
+  else 
+    Response.new(code: 404, data: "Directory doesn't exist")
+  end
 end
 
 def handle_connection(client)
